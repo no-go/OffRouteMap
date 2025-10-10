db8a7ce57a5a2809511a26b559650628a7ffea58
Jochen <jochen@win11>
2025-10-11 01:23:49 +0200

    First Features: gui zoom and theme change and store in user settings

---
+++ MainViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace OffRouteMap
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _windowTitle;
+        private double _zoomFactor;
+        private readonly ThemeService _themeService;
+        private readonly FrameworkElement _frameworkElement;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand ZoomInCommand => new RelayCommand(ZoomIn);
+        public ICommand ZoomOutCommand => new RelayCommand(ZoomOut);
+        public ICommand ToggleLightCommand => new RelayCommand(ToggleLight);
+
+
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set
+            {
+                if (value != null)
+                {
+                    _windowTitle = value;
+                    OnPropertyChanged(nameof(WindowTitle));
+                }
+            }
+        }
+
+        public double ZoomFactor
+        {
+            get { return _zoomFactor; }
+            set
+            {
+                if (_zoomFactor != value)
+                {
+                    _zoomFactor = value;
+                    OnPropertyChanged(nameof(ZoomFactor));
+                }
+            }
+        }
+
+        public MainViewModel (FrameworkElement frameworkElement) {
+            WindowTitle = GetType().Namespace;
+            this._frameworkElement = frameworkElement;
+            this._themeService = new ThemeService(255, 180, 0);
+            this._themeService.ApplyTheme(_frameworkElement, Properties.Settings.Default.isDark);
+            ZoomFactor = Properties.Settings.Default.guiSize;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void ZoomIn ()
+        {
+            ZoomFactor *= 1.1;
+            Properties.Settings.Default.guiSize = _zoomFactor;
+            Properties.Settings.Default.Save();
+        }
+
+        public void ZoomOut ()
+        {
+            ZoomFactor /= 1.1;
+            Properties.Settings.Default.guiSize = _zoomFactor;
+            Properties.Settings.Default.Save();
+        }
+
+        public void ToggleLight()
+        {
+            Properties.Settings.Default.isDark = !Properties.Settings.Default.isDark;
+            Properties.Settings.Default.Save();
+            this._themeService.ApplyTheme(_frameworkElement, Properties.Settings.Default.isDark);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # OffRouteMap
+
+A simple but modern Windows Application in C# and WPF to get and cache free
+map tiles (e.g. OSM or OpenBusMap) and use them offline to draw and messure routes
+in kilometres, save and load them.
+
+## Creation roadmap and ideas
+
+- make this readme in inital comit and make everything public on github (good idea?!)
+- cool and easy gui size and dark/light modes via MahApps.Metro
+- store gui size and theme in setting
+- store last map position in settings
+- does a nuget osm package still exist with needful features?
+- design a single window application
+- hide issues, if map title not downloadable (offline mode)
+- cache map tiles in same way like foxtrotgps in a folder
+- multithread downloads (?)
+  - use a producer/consumer pattern 
+  - maybe using LiveCharts2 to visualisze downloads
+- maybe allow to set different map tile url sources similar to foxtrotgps
+  - code must be open for that feature
+- in code docu 
+
+## Features
+
+- map
+  - [ ] given area from window size / region
+  - [ ] load tiles from given folder (settings)
+  - [ ] load tiles from osm if missing (default feature, extension possible in feature versions)
+  - [ ] move area (left click)
+  - [ ] in/out zoom (mouse wheel)
+  - [ ] display position of cursor (status line)
+- route
+  - [ ] display (or hide parts) on map in all zoom states
+  - [ ] start, end, remove and set a point/line (right click / context menu)
+  - [ ] display length (status line)
+- settings
+  - [ ] tiles folder
+  - [x] GUI size and theme (part of window frame)
+  - [ ] initial/last position
+  - [ ] initial/last zoom
+- menu
+  - [ ] load route (file window)
+  - [ ] save route (file window)
+  - [ ] set tiles folder ("file" window)
+  - [ ] clean map
+
+## Not clear and to late design questions
+
+- localisation
+- unit tests
+- something similar to doxygen
+- ci/cd
+- any idea to port it to linux? maybe with glide or something strange like fltk? a hard break between code and gui?
